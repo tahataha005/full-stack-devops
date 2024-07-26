e894e05f9f29e470beca625d4c1c97ca3d7be002
tahataha005 <taha.f.taha0@gmail.com>
2024-07-26 14:17:33 +0300

    Frontend <> Create Auth page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,19 @@
 import "./styles/App.css";
 import "./styles/index.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./ui/pages/Auth";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
