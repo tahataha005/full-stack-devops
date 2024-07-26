121396f67301237bf910b5fb0252ce5bc1106bb4
tahataha005 <taha.f.taha0@gmail.com>
2024-07-26 04:29:17 +0300

    Backend <> Install dotenv and jest

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,14 @@
 import Elysia from "elysia";
 import authApp from "./modules/auth";
+import { config } from "dotenv";
+import { connectToDatabase } from "./core/config/db/connection";
+
+config({
+  path: "./core/config/environment/env/.env",
+});
 
 new Elysia().use(authApp).listen(8080, () => {
   console.log("Server is running on port 8080");
+
+  connectToDatabase();
 });
