d954f23d8b899b7e36f88de938bdeb53027ef329
tahataha005 <taha.f.taha0@gmail.com>
2024-07-26 15:39:20 +0300

    Frontend <> Install dependencies

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,13 @@
-import "./styles/App.css";
-import "./styles/index.css";
+import "./ui/styles/colors.css";
+import "./ui/styles/App.css";
+import "./ui/styles/index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./ui/pages/Auth";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/auth" element={<Auth />} />
